f2e134c8796c5ca6c20df823e710f40cdfb61025
IIIBreakeRIII <ryu990305@icloud.com>
2025-07-16 13:49:40 +0900

    Add gitignore

---
+++ week-1/session/recursion_1.py
@@ -0,0 +1,31 @@
+# n + (n - 1) + (n - 2) + ... + 1
+def nsum(n):
+    ans = 0
+    for i in range(n):
+        ans += i
+
+    return ans + 10
+
+# nsum using Recursion
+def recusrion_nsum(n):
+    if n == 0:
+        return 0
+
+    return recusrion_nsum(n - 1) + n
+
+# num * n using recursion
+def recursion_exp(total, a, n):
+    
+    if n == 0:
+        return total
+
+    return recursion_exp(total * a, a, n - 1)
+
+# Fast Exponentiation (빠른 거듭제곱)
+def recursion_fast_exp(total, a, n):
+
+    if n == 0:
+        return 1
+    else:
+        if n % 2 == 0:
+            return recursion_fast_exp(total ** (a // 2), a, n // )
