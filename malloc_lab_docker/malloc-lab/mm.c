15b1512c1cd821e33937fcf69b2e6371cf4f9553
IIIBreakeRIII <ryu990305@icloud.com>
2025-08-24 02:25:15 +0900

    Malloc Lab : CSAPP Code

--- malloc_lab_docker/malloc-lab/mm.c
+++ malloc_lab_docker/malloc-lab/mm.c
@@ -22,17 +22,48 @@
  * NOTE TO STUDENTS: Before you do anything else, please
  * provide your team information in the following struct.
  ********************************************************/
+
+// Basic constants and macros
+// Word and header / footer size (bytes)
+#define WSIZE 4
+// Double word size (bytes)
+#define DSIZE 8
+// Extend heap by this amout (bytes)
+#define CHUNKSIZE (1 << 12)
+
+#define MAX(x, y) (x > y ? x : y)
+
+// Pack a size and allocated bit into a word
+#define PACK(size, alloc) (size | alloc)
+
+// Read and write a word at address p
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+// Read the size and allocated fields from address p
+#define GET_SIZE(p) (GET(p) & ~0x7)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+// Given block ptr bp, compute address of its header and footer
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+// Given block ptr bp, compute address of next and previous blocks
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+
 team_t team = {
-    /* Team name */
-    "ateam",
-    /* First member's full name */
-    "Harry Bovik",
-    /* First member's email address */
-    "bovik@cs.cmu.edu",
-    /* Second member's full name (leave blank if none) */
-    "",
-    /* Second member's email address (leave blank if none) */
-    ""};
+  /* Team name */
+  "ateam",
+  /* First member's full name */
+  "Harry Bovik",
+  /* First member's email address */
+  "bovik@cs.cmu.edu",
+  /* Second member's full name (leave blank if none) */
+  "",
+  /* Second member's email address (leave blank if none) */
+  ""
+};
 
 /* single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
@@ -42,54 +73,144 @@ team_t team = {
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+static void *coalesce(void *bp) {
+  size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
+  size_t next_alloc = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  size_t size = GET_SIZE(HDRP(bp));
+
+  if (prev_alloc && !next_alloc) {
+    return bp;
+  } 
+  else if (prev_alloc && !next_alloc) {
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+  } 
+  else if (!prev_alloc && next_alloc) {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  } 
+  else {
+    size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(FTRP(NEXT_BLKP(bp)));
+    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+    bp = PREV_BLKP(bp);
+  }
+  return bp;
+}
+
+
+/*
+ * extend_heap - extends the heap with a new free block
+ */
+
+static void *extend_heap(size_t words) {
+  char *bp;
+  size_t size;
+
+  // Allocate an even number of words to maintain alignment
+  size = (words % 2) ? (words + 1) * WSIZE : words * WSIZE;
+  
+  if((long)(bp = mem_sbrk(size)) == -1) {
+    return NULL;
+  }
+
+  // Initialize free block header / footer and the epilogue header
+  // Free block header
+  PUT(HDRP(bp), PACK(size, 0));
+  // Free block footer
+  PUT(FTRP(bp), PACK(size, 0));
+  // New epilogue header
+  PUT(NEXT_BLKP(bp), PACK(0, 1));
+
+  // Coalesce if the previous block was free
+  return coalesce(bp);
+}
+
 /*
  * mm_init - initialize the malloc package.
+ * malloc 패키지의 초기화
  */
-int mm_init(void)
-{
-    return 0;
+int mm_init(void) {
+
+  // 초기 힙 생성
+  char *heap_listp;
+
+  // Create the initial empty heap
+  if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *) - 1) {
+    return -1;
+  }
+  
+  // Alignment padding
+  PUT(heap_listp, 0);
+
+  // Prologue header
+  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+
+  // Prologue footer
+  PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+
+  // Epilogue header
+  PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+
+  heap_listp += (2 * WSIZE);
+
+  // Extend the empty heap with a free block of CHUNKSIZE bytes
+  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
+    return -1;
+  }
+
+  return 0;
 }
 
 /*
  * mm_malloc - Allocate a block by incrementing the brk pointer.
- *     Always allocate a block whose size is a multiple of the alignment.
+ * Always allocate a block whose size is a multiple of the alignment.
+ * malloc의 구현 : 할당
  */
-void *mm_malloc(size_t size)
-{
-    int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
-    if (p == (void *)-1)
-        return NULL;
-    else
-    {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
-    }
+void *mm_malloc(size_t size) {
+  int newsize = ALIGN(size + SIZE_T_SIZE);
+  void *p = mem_sbrk(newsize);
+  if (p == (void *) - 1)
+    return NULL;
+  else {
+    *(size_t *)p = size;
+    return (void *)((char *)p + SIZE_T_SIZE);
+  }
 }
 
 /*
  * mm_free - Freeing a block does nothing.
+ * malloc의 해제
  */
-void mm_free(void *ptr)
-{
+void mm_free(void *bp) {
+  size_t size = GET_SIZE(HDRP(bp));
+
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+  coalesce(bp);
 }
 
 /*
  * mm_realloc - Implemented simply in terms of mm_malloc and mm_free
+ * malloc의 재할당
  */
-void *mm_realloc(void *ptr, size_t size)
-{
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
+void *mm_realloc(void *ptr, size_t size) {
+  void *oldptr = ptr;
+  void *newptr;
+  size_t copySize;
 
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-        return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-        copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+  newptr = mm_malloc(size);
+  if (newptr == NULL) {
+    return NULL;
+  }
+  copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+  if (size < copySize) {
+    copySize = size;
+  }
+  memcpy(newptr, oldptr, copySize);
+  mm_free(oldptr);
+  return newptr;
 }
