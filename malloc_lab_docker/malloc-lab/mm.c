126dd5a754b634bf2f5673987aced23b199a7312
IIIBreakeRIII <ryu990305@icloud.com>
2025-08-25 02:55:57 +0900

    Malloc Lab init, mm_malloc description

--- malloc_lab_docker/malloc-lab/mm.c
+++ malloc_lab_docker/malloc-lab/mm.c
@@ -18,59 +18,44 @@
 #include "mm.h"
 #include "memlib.h"
 
-/*********************************************************
- * NOTE TO STUDENTS: Before you do anything else, please
- * provide your team information in the following struct.
- ********************************************************/
-
-// Basic constants and macros
-// Word and header / footer size (bytes)
-#define WSIZE 4
-// Double word size (bytes)
-#define DSIZE 8
-// Extend heap by this amout (bytes)
-#define CHUNKSIZE (1 << 12)
-
-#define MAX(x, y) (x > y ? x : y)
-
-// Pack a size and allocated bit into a word
-#define PACK(size, alloc) (size | alloc)
-
-// Read and write a word at address p
-#define GET(p) (*(unsigned int *)(p))
-#define PUT(p, val) (*(unsigned int *)(p) = (val))
-
-// Read the size and allocated fields from address p
-#define GET_SIZE(p) (GET(p) & ~0x7)
-#define GET_ALLOC(p) (GET(p) & 0x1)
-
-// Given block ptr bp, compute address of its header and footer
-#define HDRP(bp) ((char *)(bp) - WSIZE)
-#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
-
-// Given block ptr bp, compute address of next and previous blocks
-#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
-#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
-
 team_t team = {
-  /* Team name */
-  "ateam",
-  /* First member's full name */
-  "Harry Bovik",
-  /* First member's email address */
-  "bovik@cs.cmu.edu",
-  /* Second member's full name (leave blank if none) */
+  "Team 6",
+  "Dev/ Paul",
+  "ryu990305@gmail.com",
   "",
-  /* Second member's email address (leave blank if none) */
   ""
 };
 
-/* single word (4) or double word (8) alignment */
+// Initial Constant
+#define WSIZE 4               // Word Size - bytes
+#define DSIZE 8               // Double Word Size - bytes
+#define CHUNKSIZE (1 << 12)   // 힙 확장을 위한 기본 크기 - bytes
+
+// Macro Parts
+#define MAX(x, y) (x > y ? x : y)
+// size(블록 크기), alloc(할당 비트)
+#define PACK(size, alloc) (size | alloc)
+// 주소 p가 참조하는 워드 - read
+#define GET(p) (*(unsigned int *)(p))
+// GET 한 주소 p를 val로 반환
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+// 주소 p의 크기 - read(조건: 0111와 &연산)
+#define GET_SIZE(p) (GET(p) & ~0x7)
+// 주소 p의 할당
+#define GET_ALLOC(p) (GET(p) & 0x1)
+// Header Pointer, “bp가 payload 시작을 가리키니까, header는 그 앞부분이니 WSIZE(=4바이트)만큼 빼준다”
+#define HDRP(bp) ((char *)(bp) - WSIZE)
+// Footer Pointer "bp + {전체 블록 크기(hdr + pyld + ftr) - (hdr + ftr)}"
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+// 다음 블록의 포인터
+#define NEXT_BLKP(bp) ((char *)(bp) + GET_SIZE(((char *)(bp) - WSIZE)))
+// 이전 블록의 포인터
+#define PREV_BLKP(bp) ((char *)(bp) - GET_SIZE(((char *)(bp) - DSIZE)))
+// 메모리 정렬: 8바이트 단위로 정렬되어야 함
 #define ALIGNMENT 8
-
-/* rounds up to the nearest multiple of ALIGNMENT */
+// 주어진 size를 가장 가까운 8의 배수로 올림(&~0x7 = 하위 3비트를 연산)
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
-
+// 정렬 안정을 위한 8바이트 연산
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
 static void *coalesce(void *bp) {
@@ -129,62 +114,45 @@ static void *extend_heap(size_t words) {
   return coalesce(bp);
 }
 
-/*
- * mm_init - initialize the malloc package.
- * malloc 패키지의 초기화
- */
 int mm_init(void) {
 
   // 초기 힙 생성
   char *heap_listp;
 
-  // Create the initial empty heap
-  if ((heap_listp = mem_sbrk(4*WSIZE)) == (void *) - 1) {
-    return -1;
-  }
-  
-  // Alignment padding
+  // 비어있는 힙 생성 (4 * 4 = 16bytes): 프롤로그 블록을 구성하기 위해
+  if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) - 1) { return -1; }
+  // 정렬용 패딩
   PUT(heap_listp, 0);
-
   // Prologue header
   PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
-
   // Prologue footer
   PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
-
   // Epilogue header
   PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
 
-  heap_listp += (2 * WSIZE);
+  // heap_listp += (2 * WSIZE);
 
-  // Extend the empty heap with a free block of CHUNKSIZE bytes
-  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) {
-    return -1;
-  }
+  // 힙의 확장을 위해 - 1024워드로
+  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) { return -1; }
 
   return 0;
 }
 
-/*
- * mm_malloc - Allocate a block by incrementing the brk pointer.
- * Always allocate a block whose size is a multiple of the alignment.
- * malloc의 구현 : 할당
- */
 void *mm_malloc(size_t size) {
+  // 올림 진행: 사용자 요청 크기(payload) + 최소 8비트(header + sorting)
   int newsize = ALIGN(size + SIZE_T_SIZE);
+  // 힙 공간을 newsize만큼 늘림
   void *p = mem_sbrk(newsize);
-  if (p == (void *) - 1)
-    return NULL;
+  // p == (void *) - 1 : 오류 시에 쓰는 특별한 센티넬 값 (전통적인 sbrk()의 관례)
+  if (p == (void *) - 1) { return NULL; }
   else {
+    // p위치에 요청된 크기 저장
     *(size_t *)p = size;
+    // payload의 시작값을 반환하기 위해, 헤더를 포함한 주소 시작 + SIZE_T_SIZE
     return (void *)((char *)p + SIZE_T_SIZE);
   }
 }
 
-/*
- * mm_free - Freeing a block does nothing.
- * malloc의 해제
- */
 void mm_free(void *bp) {
   size_t size = GET_SIZE(HDRP(bp));
 
