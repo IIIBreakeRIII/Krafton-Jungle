90c634b621de270bf7a28004de0254efe2d45184
IIIBreakeRIII <ryu990305@icloud.com>
2025-08-25 15:34:17 +0900

    First-Fit Clear

--- malloc_lab_docker/malloc-lab/mm.c
+++ malloc_lab_docker/malloc-lab/mm.c
@@ -31,6 +31,81 @@ team_t team = {
 #define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
 
+static void *heap_listp; 
+static void *extend_heap(size_t words);
+static void *coalesce(void *bp);
+static void *find_fit(size_t asize);
+static void place(void *bp, size_t asize);
+
+// mm_init
+int mm_init(void) {
+
+  if ((heap_listp = mem_sbrk(4 * WSIZE)) == (void *) - 1) { return -1; }
+
+  PUT(heap_listp, 0);
+  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
+  PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
+
+  heap_listp += (2 * WSIZE);
+
+  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) { return -1; }
+
+  return 0;
+}
+
+// mm_malloc
+void *mm_malloc(size_t size) {
+  size_t asize;
+  size_t extendsize;
+  char *bp;
+
+  if (size == 0) { return NULL; }
+
+  if (size <= DSIZE) { asize = 2 * DSIZE; }
+  else { asize = DSIZE * ((size + DSIZE + (DSIZE - 1)) / DSIZE); }
+
+  if ((bp = find_fit(asize)) != NULL) {
+    place(bp, asize);
+    return bp;
+  }
+
+  extendsize = MAX(asize, CHUNKSIZE);
+
+  if ((bp = extend_heap(extendsize/WSIZE)) == NULL) { return NULL; }
+  
+  place(bp, asize);
+
+  return bp;
+}
+
+// mm_free
+void mm_free(void *bp) {
+  size_t size = GET_SIZE(HDRP(bp));
+
+  PUT(HDRP(bp), PACK(size, 0));
+  PUT(FTRP(bp), PACK(size, 0));
+
+  coalesce(bp);
+}
+
+// mm_realloc
+void *mm_realloc(void *ptr, size_t size) {
+  if (ptr == NULL) return mm_malloc(size);
+  if (size == 0) { mm_free(ptr); return NULL; }
+
+  void *oldptr = ptr;
+  void *newptr = mm_malloc(size);
+  if (newptr == NULL) { return NULL; }
+
+  size_t copySize = GET_SIZE(HDRP(oldptr)) - DSIZE;
+  if (size < copySize) { copySize = size; }
+
+  memcpy(newptr, oldptr, copySize);
+  mm_free(oldptr);
+  return newptr;
+}
+
 // merge - coalesce
 static void *coalesce(void * bp) {
   size_t prev_alloc = GET_ALLOC(FTRP(PREV_BLKP(bp)));
@@ -47,10 +122,11 @@ static void *coalesce(void * bp) {
   else if (!prev_alloc && next_alloc) {
     size += GET_SIZE(HDRP(PREV_BLKP(bp)));
 
-    PUT(FTRP(bp), PACK(size, 0));
-    PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
-
     bp = PREV_BLKP(bp);
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    // bp = PREV_BLKP(bp);
   }
   else {
     size += GET_SIZE(HDRP(PREV_BLKP(bp))) + GET_SIZE(HDRP(NEXT_BLKP(bp)));
@@ -79,60 +155,30 @@ static void *extend_heap(size_t words) {
   return coalesce(bp);
 }
 
-// mm_init
-int mm_init(void) {
-  char *heap_listp;
+// find_fit
+static void *find_fit(size_t asize) {
+  void *bp = mem_heap_lo() + 2 * WSIZE;
 
-  if ((heap_listp == mem_sbrk(4 * WSIZE)) == (void *) - 1) { return -1; }
-
-  PUT(heap_listp, 0);
-  PUT(heap_listp + (1 * WSIZE), PACK(DSIZE, 1));
-  PUT(heap_listp + (2 * WSIZE), PACK(DSIZE, 1));
-  PUT(heap_listp + (3 * WSIZE), PACK(0, 1));
-
-  if (extend_heap(CHUNKSIZE / WSIZE) == NULL) { return -1; }
-
-  return 0;
-}
-
-// mm_malloc
-void *mm_malloc(size_t size) {
-  int newsize = ALIGN(size + SIZE_T_SIZE);
-  void *p = mem_sbrk(newsize);
-
-  if (p == (void *) - 1) { return NULL; }
-  else {
-    *(size_t *)p = size;
-    return (void *)((char *)p + SIZE_T_SIZE);
+  for (bp = heap_listp; GET_SIZE(HDRP(bp)) > 0; bp = NEXT_BLKP(bp)) {
+    if (!GET_ALLOC(HDRP(bp)) && (asize <= GET_SIZE(HDRP(bp)))) { return bp; }
   }
+
+  return NULL;
 }
 
-// mm_free
-void mm_free(void *bp) {
-  size_t size = GET_SIZE(HDRP(bp));
+// place
+static void place(void *bp, size_t asize) {
+  size_t csize = GET_SIZE(HDRP(bp));
 
-  PUT(HDRP(bp), PACK(size, 0));
-  PUT(FTRP(bp), PACK(size, 0));
+  if ((csize - asize) >= (2 * DSIZE)) {
+    PUT(HDRP(bp), PACK(asize, 1));
+    PUT(FTRP(bp), PACK(asize, 1));
 
-  coalesce(bp);
-}
-
-// mm_realloc
-void *mm_realloc(void *ptr, size_t size) {
-  void *oldptr = ptr;
-  void *newptr;
-  size_t copysize;
-
-  newptr = mm_malloc(size);
-
-  if (newptr == NULL) { return NULL; }
-
-  copysize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-
-  if (size < copysize) { copysize = size; }
-
-  memcpy(newptr, oldptr, copysize);
-  mm_free(oldptr);
-
-  return newptr;
+    PUT(HDRP(NEXT_BLKP(bp)), PACK((csize - asize), 0));
+    PUT(FTRP(NEXT_BLKP(bp)), PACK((csize - asize), 0));
+  }
+  else {
+    PUT(HDRP(bp), PACK(csize, 1));
+    PUT(FTRP(bp), PACK(csize, 1));
+  }
 }
