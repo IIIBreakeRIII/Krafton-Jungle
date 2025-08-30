48c76b5015a202a3bf82398c8a831efd3450c1c2
IIIBreakeRIII <ryu990305@icloud.com>
2025-08-30 23:46:07 +0900

    Complete Tiny: Need Code Reading

--- webproxy_lab_docker/webproxy-lab/tiny/cgi-bin/adder.c
+++ webproxy_lab_docker/webproxy-lab/tiny/cgi-bin/adder.c
@@ -2,7 +2,7 @@
  * adder.c - a minimal CGI program that adds two numbers together
  */
 /* $begin adder */
-#include "csapp.h"
+#include "../csapp.h"
 
 int main(void)
 {
@@ -25,8 +25,7 @@ int main(void)
   sprintf(content, "QUERY_STRING=%s\r\n<p>", buf);
   sprintf(content + strlen(content), "Welcome to add.com: ");
   sprintf(content + strlen(content), "THE Internet addition portal.\r\n<p>");
-  sprintf(content + strlen(content), "The answer is: %d + %d = %d\r\n<p>",
-          n1, n2, n1 + n2);
+  sprintf(content + strlen(content), "The answer is: %d + %d = %d\r\n<p>", n1, n2, n1 + n2);
   sprintf(content + strlen(content), "Thanks for visiting!\r\n");
 
   /* Generate the HTTP response */
